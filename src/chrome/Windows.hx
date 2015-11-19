713beb4e0a2267fbcd11379b2ea7c2d4ecdae60f
Ryan Davis <epikit@gmail.com>
2015-11-18 18:06:49 -0600

    Created chrome.Windows class

---
+++ src/chrome/Windows.hx
@@ -0,0 +1,19 @@
+package chrome;
+
+import tannus.ds.Promise;
+
+import chrome.app.AppWindow.CreateWindowOptions;
+import chrome.app.AppWindow;
+
+class Windows {
+
+	/**
+	  * Create a new AppWindow
+	  */
+	public static function create(url:String, ?options:CreateWindowOptions, ?cb:AppWindow->Void):Void {
+		lib.create(url, options, cb);
+	}
+	
+	/* Reference to chrome.app.window to be used internally */
+	private static var lib:Dynamic = {untyped __js__('chrome.app.window');};
+}
